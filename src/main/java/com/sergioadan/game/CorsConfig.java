ca553fa9a1eeb206b8003152b0ae0e55295e0d2d
DESKTOP-VVRH4KM\sergi <sergiioadan@hotmail.com>
2025-06-22 21:55:05 +0200

    feat: add front url

--- src/main/java/com/sergioadan/game/CorsConfig.java
+++ src/main/java/com/sergioadan/game/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins("http://localhost:4200","https://code-battle-front.onrender.com")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
